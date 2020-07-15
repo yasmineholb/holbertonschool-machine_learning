e05319e9aae023ec222d44cb9d75949e84536009
yasmineholb <1055@holbertonschool.com>
2020-07-15 09:32:41 -0500

    fix

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
-""" cat fn """
-import copy
-
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """ cat matrices """
-    matt1 = copy.deepcopy(mat1)
-    matt2 = copy.deepcopy(mat2)
     if axis == 0:
-        if len(matt1[0]) == len(matt2[0]):
-            m = matt1 + matt2
+        if len(mat1[0]) == len(mat2[0]):
+            m = mat1 + mat2
             return(m)
         else:
             return(None)
     elif axis == 1:
-        if len(matt1) == len(matt2):
+        if len(mat1) == len(mat2):
             n = []
-            for i in range(len(matt1)):
-                m = matt1[i] + matt2[i]
+            for i in range(len(mat1)):
+                m = mat1[i] + mat2[i]
                 n.append(m)
             return(n)
         else:
