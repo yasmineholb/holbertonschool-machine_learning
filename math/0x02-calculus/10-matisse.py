ed953c3eccb31d9ef692b3911923ae09737b297d
yasmineholb <1055@holbertonschool.com>
2020-07-25 06:45:46 -0500

    test

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" derivate function"""
+
+
+def poly_derivative(poly):
+    x = 0
+    m = []
+    p = [0]
+    if len(poly) == 1:
+        return(p)
+    if poly is None:
+        return None
+    for i in range(1, len(poly)):
+        x = poly[i] * i
+        m.append(x)
+    return m
