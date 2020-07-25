2fee5f54207c6752063f87a57134291bbe765336
yasmineholb <1055@holbertonschool.com>
2020-07-25 16:41:15 -0500

    fix

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -12,9 +12,6 @@ def poly_integral(poly, C=0):
         return None
     lis = [C]
     for i in range(len(poly)):
-        if type(poly[i]) is not int and type(poly[i]) is not float:
-            return(None)
-    for i in range(len(poly)):
         if int(poly[i]/(i+1)) == poly[i]/(i+1):
             lis.append(int(poly[i]/(i+1)))
         else:
