9808eabc4e39fd7c90ee0c381e22811e25388d39
yasmineholb <1055@holbertonschool.com>
2020-07-25 13:16:28 -0500

    test

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """sum function """
-    if type(n) is not int or n is None or n < 1:
+    if n is None or n < 1:
         return(None)
     return(int(((1/3)*(n**3))+((1/2)*(n**2))+(1/6)*n))
