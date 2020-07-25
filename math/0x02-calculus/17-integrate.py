00bdd48cd61bf605d93143eed1c9bc3f132f5c90
yasmineholb <1055@holbertonschool.com>
2020-07-25 08:29:57 -0500

    tes

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -10,6 +10,9 @@ def poly_integral(poly, C=0):
         return None
     elif type(poly) == list:
         lis = [C]
+    for i in range(len(poly)):
+        if type(poly[i]) is not int:
+            return(None)
         for i in range(len(poly)):
             if int(poly[i]/(i+1)) == poly[i]/(i+1):
                 lis.append(int(poly[i]/(i+1)))
