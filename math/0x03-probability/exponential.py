1f2773aa30e376157cb5016a91530d8918cafe68
yasmineholb <1055@holbertonschool.com>
2020-07-29 12:29:42 -0500

    test

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -28,9 +28,9 @@ class Exponential:
 
     def cdf(self, x):
         """cdf"""
+        p = 1 - (2.7182818285**(-self.lambtha * x))
         if x < 0:
             return 0
-        p = 1 - (2.7182818285**(-self.lambtha * x))
         elif x >= 0:
             return p
         else:
