9c88c45a51366beefd18b87cbad256ab1a412151
yasmineholb <1055@holbertonschool.com>
2020-07-29 05:22:57 -0500

    fix

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -11,10 +11,10 @@ class Poisson:
         if data is None:
             data = lambtha
             if lambtha < 0:
-                raise TypeError("lambtha must be a positive value")
+                raise ValueError("lambtha must be a positive value")
         elif data:
             self.lambtha = sum(data)/len(data)
             if type(data) is not list:
                 raise TypeError("data must be a list")
             elif len(data) < 2:
-                raise TypeError("data must contain multiple values")
+                raise ValueError("data must contain multiple values")
