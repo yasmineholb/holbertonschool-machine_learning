d2291740d5a4b40bde1af346663520c05639d4d3
yasmineholb <1055@holbertonschool.com>
2020-07-29 11:19:27 -0500

    test

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -20,7 +20,7 @@ class Exponential:
 
     def pdf(self, x):
         """ pdf """
-        if x == 0:
+        if x == self.lambtha:
             return 0
         p = self.lambtha * (2.7182818285**(-self.lambtha * x))
         return(p)
