f7413272707cee11f04ef0b4f586b7e1184736d5
yasmineholb <1055@holbertonschool.com>
2020-07-29 06:41:46 -0500

    poisson

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,14 +6,14 @@ class Poisson:
     """class"""
     def __init__(self, data=None, lambtha=1.):
         """ function """
-        self.data = data
+        """self.data = data"""
         self.lambtha = float(lambtha)
         if data is None:
-            data = lambtha
+            data = self.lambtha
             if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
         elif data:
-            self.lambtha = sum(data)/len(data)
+            self.lambtha = float(sum(data)/len(data))
             if type(data) is not list:
                 raise TypeError("data must be a list")
             elif len(data) < 2:
