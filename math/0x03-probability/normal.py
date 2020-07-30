d1a8ce7bea84394295243e4184a4735758327e3e
yasmineholb <1055@holbertonschool.com>
2020-07-30 06:06:22 -0500

    test

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -36,3 +36,14 @@ class Normal:
         pi = 3.1415926536
         e = 2.7182818285
         return (1 / (self.stddev * ((2 * pi)**0.5)) * (e ** ((-0.5) * p)))
+
+    def erf(self, x):
+        """ erf function """
+        p = 3.1415926536
+        return (2/(p**0.5))*(x-((x**3)/3)+((x**5)/10)-((x**7)/42)+((x**9)/216))
+
+    def cdf(self, x):
+        """ CDF function """
+        p = (self.z_score(x))/(2**0.5)
+        pi = 3.1415926536
+        return (1+self.erf(p))/2
