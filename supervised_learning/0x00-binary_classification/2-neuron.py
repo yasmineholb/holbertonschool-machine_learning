b999725a91144ba1775e4381b9d06131e69a12c5
yasmineholb <1055@holbertonschool.com>
2020-08-05 07:59:04 -0500

    tes

---
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" new class """
+import numpy as np
+
+
+class Neuron():
+    """ class neuron """
+    def __init__(self, nx):
+        """ init function """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ getter W """
+        return self.__W
+
+    @property
+    def b(self):
+        """ getter b """
+        return self.__b
+
+    @property
+    def A(self):
+        """ getter A """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ forward function """
+        nx, m = np.shape(X)
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1/(1 + (2.7182818285**(-z)))
+        return self.__A
