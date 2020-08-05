9f62e226b1600916842be40806144990af87f6dd
yasmineholb <1055@holbertonschool.com>
2020-08-05 07:17:23 -0500

    fix

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -11,7 +11,6 @@ class Neuron():
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise TypeError("nx must be a positive integer")
-        self.nx = nx
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
