578667b2cdeb62e4acdaa4935962e8aa23de9565
yasmineholb <1055@holbertonschool.com>
2020-08-05 16:32:42 -0500

    test

---
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" new class """
+import numpy as np
+
+
+class Neuron():
+    """ class neuron """
+    def __init__(self, nx):
+        """ init function """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ getter W """
+        return self.__W
+
+    @property
+    def b(self):
+        """ getter b """
+        return self.__b
+
+    @property
+    def A(self):
+        """ getter A """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ forward function """
+        nx, m = np.shape(X)
+        x = np.matmul(self.__W, X) + self.__b
+        self.__A = 1/(1 + (np.exp(-x)))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ cost function """
+        m = A.shape[1]
+        print(m)
+        cos = (-np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))) / m
+        return cos
