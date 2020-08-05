f8bec5aa7e07a2f52fdf1d2038f0e81fed149d92
yasmineholb <1055@holbertonschool.com>
2020-08-05 16:37:36 -0500

    test

--- supervised_learning/0x00-binary_classification/3-neuron.py
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -40,6 +40,5 @@ class Neuron():
     def cost(self, Y, A):
         """ cost function """
         m = Y.shape[1]
-        print(m)
-        cos = (-(np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)))) / m
-        return cos
+        cos = -(np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))) 
+        return cos/m
