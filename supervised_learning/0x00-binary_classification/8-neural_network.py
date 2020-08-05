886131896287c47d13381ee2149e97966e0ba350
yasmineholb <1055@holbertonschool.com>
2020-08-05 08:38:37 -0500

    network

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -9,11 +9,11 @@ class NeuralNetwork():
         """ init function """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        elif nx < 1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
         if type(nodes) is not int:
             raise TypeError("nodes must be an integer")
-        elif nodes < 1:
+        if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = [[0] for i in [nodes] * nodes]
