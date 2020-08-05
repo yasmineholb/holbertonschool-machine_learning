7b3c08fcb992c9a1bfc10d09d48735e8b95f004a
yasmineholb <1055@holbertonschool.com>
2020-08-05 08:02:52 -0500

    tes

--- supervised_learning/0x00-binary_classification/2-neuron.py
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -33,6 +33,6 @@ class Neuron():
     def forward_prop(self, X):
         """ forward function """
         nx, m = np.shape(X)
-        z = np.matmul(self.__W, X) + self.__b
-        self.__A = 1/(1 + (np.exp(-z)))
+        x = np.matmul(self.__W, X) + self.__b
+        self.__A = 1/(1 + (np.exp(-x)))
         return self.__A
