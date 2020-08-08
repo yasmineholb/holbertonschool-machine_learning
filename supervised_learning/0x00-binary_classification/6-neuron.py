7aae9c498108bd72d9ea176ddaaea02934edf400
yasmineholb <1055@holbertonschool.com>
2020-08-08 03:39:47 -0500

    check

--- supervised_learning/0x00-binary_classification/6-neuron.py
+++ supervised_learning/0x00-binary_classification/6-neuron.py
@@ -51,25 +51,24 @@ class Neuron():
 
     def gradient_descent(self, X, Y, A, alpha=0.05):
         """ gradient descent function """
-        m = Y.shape[1]
-        p = self.forward_prop(X) - Y
-        self.__W = self.__W - (alpha/m) * np.sum((p)*X, axis=1)
-        self.__b = self.__b - (alpha/m) * sum(sum(A - Y))
+        nx, m = np.shape(X)
+        db = np.sum(A-Y)
+        dW = np.sum((A-Y)*X, axis=1)
+        self.__W = self.__W - (alpha/m) * dW
+        self.__b = self.__b - (alpha/m) * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """ Training """
-        nx, m = np.shape(X)
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
-        if iterations < 0:
+        elif iterations < 1:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise TypeError("alpha must be a float")
-        if alpha < 0:
-            raise ValueError("alpha must be positive")
+        elif alpha <= 0:
+            raise ValueError("nx must be  positive")
+        nx, m = np.shape(X)
         for i in range(iterations):
-            p = self.forward_prop(X)-Y
-            self.__W = self.__W - (alpha/m) * np.sum((p)*X, axis=1)
-            self.__b = self.__b - (alpha/m) * sum(sum(self.__A-Y))
             self.__A = self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A, alpha=0.05)
         return np.round(self.__A).astype(np.int), self.cost(Y, self.__A)
