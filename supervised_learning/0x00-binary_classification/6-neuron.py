1eb784e06adf6fd5a0f6a9f3bbbf981b20b65016
yasmineholb <1055@holbertonschool.com>
2020-08-08 06:27:49 -0500

    test

--- supervised_learning/0x00-binary_classification/6-neuron.py
+++ supervised_learning/0x00-binary_classification/6-neuron.py
@@ -65,7 +65,7 @@ class Neuron():
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise TypeError("alpha must be a float")
-        if alpha < 0:
+        if alpha <= 0:
             raise ValueError("alpha must be positive")
         nx, m = np.shape(X)
         for i in range(iterations):
