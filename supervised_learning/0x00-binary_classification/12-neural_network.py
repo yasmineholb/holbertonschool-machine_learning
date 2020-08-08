4d63e4f89be8bdb2be1e6f03bc7d1ad6cf9b2902
yasmineholb <1055@holbertonschool.com>
2020-08-08 12:42:24 -0500

    check

---
+++ supervised_learning/0x00-binary_classification/12-neural_network.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+""" new class """
+import numpy as np
+
+
+class NeuralNetwork():
+    """ neural network class"""
+    def __init__(self, nx, nodes):
+        """ init function """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros(shape=(nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """ self fn """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """ self fn """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """ self fn """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """ self fn """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """ self fn """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """ self fn """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """ forward function """
+        nx, m = np.shape(X)
+        x1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1/(1 + (np.exp(-(x1))))
+        x2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1/(1 + (np.exp(-(x2))))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """ cost function """
+        m = Y.shape[1]
+        cos = -(np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)))
+        return cos/m
+
+    def evaluate(self, X, Y):
+        """ evaluate function """
+        z, p = self.forward_prop(X)
+        t = np.round(p)
+        return t.astype(np.int), self.cost(Y, p)
