c0957d653ef44c56034ec77b1a42ce7663932dd5
yasmineholb <1055@holbertonschool.com>
2020-08-08 11:12:29 -0500

    test

--- supervised_learning/0x00-binary_classification/5-neuron.py
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -58,7 +58,7 @@ class Neuron():
     def gradient_descent(self, X, Y, A, alpha=0.05):
         """ gradient descent function """
         nx, m = np.shape(X)
-        db = np.sum(A-Y)
         dW = np.sum((A-Y)*X, axis=1)
+        db = np.sum(A-Y)
         self.__W = self.__W - (alpha/m) * dW
         self.__b = self.__b - (alpha/m) * db
