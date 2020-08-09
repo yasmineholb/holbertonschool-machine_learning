6988aec213bec39038df3b62589bd7a0f67f64e9
yasmineholb <1055@holbertonschool.com>
2020-08-09 17:37:23 -0500

    test

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -20,11 +20,10 @@ class DeepNeuralNetwork():
         self.cache = {}
         self.weights = {}
         for i in range(self.L):
+            self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
             if i > 0:
-                self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
                 self.weights["W" + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
             else:
                 self.weights["W1"] = np.random.randn(layers[0], nx) * np.sqrt(
                     2 / nx)
-                self.weights["b1"] = np.zeros((layers[0], 1))
