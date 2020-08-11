38fbc9a26a4a356ba61e70d3a99b3cddaa30c312
yasmineholb <1055@holbertonschool.com>
2020-08-11 22:50:07 +0200

    chek

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -7,12 +7,6 @@ def one_hot_encode(Y, classes):
     """ one hot """
     if Y is None or classes is None:
         return None
-    if len(Y) == 0:
-        return None
-    if classes < 0:
-        return None
-    if type(classes) is not int:
-        return None
     try:
         """b = np.zeros((Y.size, classes))
         b[np.arange(Y.size), Y] = 1
