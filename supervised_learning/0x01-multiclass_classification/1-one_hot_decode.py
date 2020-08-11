8dd7d17e5862714e9ed1bee1dafbf5f1dc7dd62d
yasmineholb <1055@holbertonschool.com>
2020-08-11 23:52:28 +0200

    tets

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -9,4 +9,8 @@ def one_hot_decode(one_hot):
         return None
     if type(one_hot) is not np.ndarray:
         return None
-    return np.argmax(one_hot, axis=0)
+    try:
+        s = np.argmax(one_hot, axis=0)
+        return s
+    except Exception as error:
+        return None
