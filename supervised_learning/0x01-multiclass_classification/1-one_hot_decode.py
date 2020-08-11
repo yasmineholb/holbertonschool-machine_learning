68b1262fc00cdc1338bf7ef936aa08be17adbb5e
yasmineholb <1055@holbertonschool.com>
2020-08-11 23:39:08 +0200

    tets

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -5,8 +5,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ one hot decode """
-    if one_hot is None:
-        return None
     if len(one_hot.shape) != 2:
         return None
     if type(one_hot) is not np.ndarray:
