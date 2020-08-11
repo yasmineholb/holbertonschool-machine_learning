7676a697e3af9eded2077589db0f62b6970967a2
yasmineholb <1055@holbertonschool.com>
2020-08-12 00:04:12 +0200

    test

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -5,13 +5,9 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ one hot decode """
-    if len(one_hot.shape) != 2:
-        return None
     if type(one_hot) is not np.ndarray:
         return None
-    if (one_hot < 0).all():
-        return None
-    if (one_hot > 1).all():
+    if len(one_hot.shape) != 2:
         return None
     s = np.argmax(one_hot, axis=0)
     return s
