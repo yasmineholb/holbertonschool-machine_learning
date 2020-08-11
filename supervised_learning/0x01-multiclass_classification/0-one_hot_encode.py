3f8e5602687282f0ae07304e017d98bb74bfcf07
yasmineholb <1055@holbertonschool.com>
2020-08-11 22:27:45 +0200

    test

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -5,6 +5,12 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ one hot """
+    if Y is None or classes is None:
+        return None
+    if len(Y) == 0:
+        return None
+    if classes < 0:
+        return None
     b = np.zeros((Y.size, classes+1))
     b[np.arange(Y.size), Y] = 1
     return b
