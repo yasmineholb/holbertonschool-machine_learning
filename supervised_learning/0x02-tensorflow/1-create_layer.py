026f287aa0ff704448dbdd4d03a2986a1480b80d
yasmineholb <1055@holbertonschool.com>
2020-08-15 10:49:56 +0200

    test

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" create function """
+import tensorflow as tf
+import numpy as np
+
+
+def create_layer(prev, n, activation):
+    """ create layer function """
+    linear_model = tf.layers.dense(prev, units=n, activation=activation,
+                                   name="layer")
+    init = tf.contrib.layers.variance_scaling_initializer(prev, mode="FAN_AVG")
+    """sess = tf.Session()
+    sess.run(init) """
+    return(linear_model)
