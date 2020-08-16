c2a51629e64ea0beb099870372562083eddc39bc
yasmineholb <1055@holbertonschool.com>
2020-08-17 01:01:31 +0200

    add

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
           alpha, iterations, save_path="/tmp/model.ckpt"):
+    """ train function """
     calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
     calculate_loss = __import__('4-calculate_loss').calculate_loss
     create_placeholders = __import__(
