63c1698e7837306cdae5b47800040d096fdac58c
yasmineholb <1055@holbertonschool.com>
2020-08-16 17:12:10 +0200

    test

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -9,7 +9,12 @@ def calculate_accuracy(y, y_pred):
     cost =tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
     logits = y_pred, labels = y))
     acc = tf.reduce_mean(tf.cast(ac, tf.float32))
-    return acc"""
+    return acc
     ac = tf.equal(y, y_pred)
     accuracy = tf.reduce_mean(tf.cast(ac, tf.float32))
+    return accuracy"""
+    diff = tf.math.abs(y - y_pred)
+    decimal = tf.constant(0.1)
+    correct = tf.math.less(diff, decimal)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
     return accuracy
