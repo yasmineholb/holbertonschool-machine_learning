eb3438ceecb9214bea80f11e7dd7293ec8d37c39
yasmineholb <1055@holbertonschool.com>
2020-08-16 11:27:14 +0200

    test

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -5,9 +5,11 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     """ create layer function """
+    scope = tf.variable_scope(None, default_name='layer').__enter__()
     kernel = tf.contrib.layers.variance_scaling_initializer(factor=2.0,
                                                             mode="FAN_AVG")
     linear_model = tf.layers.dense(prev, units=n, activation=activation,
                                    kernel_initializer=kernel,
                                    name="layer", reuse=tf.AUTO_REUSE)
+    scope.reuse_variables()
     return(linear_model)
