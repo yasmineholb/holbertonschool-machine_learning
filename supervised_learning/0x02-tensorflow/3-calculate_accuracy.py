e906b3ece93ccdd8ade610f2ee75af2b129ea162
yasmineholb <1055@holbertonschool.com>
2020-08-16 12:52:13 +0200

    calculate

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,5 +5,9 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ calculate function """
-    acc = tf.metrics.accuracy(y, predictions=y_pred)
+    """acc = tf.metrics.accuracy(y, predictions=y_pred)"""
+    """cost =tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits = y_pred, labels = y))"""
+    acc = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+        logits=y_pred, labels=y))
     return acc
