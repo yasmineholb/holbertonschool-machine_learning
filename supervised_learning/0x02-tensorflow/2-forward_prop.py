0e32351eb1ed8ce8680135698d81eba863a35f5c
yasmineholb <1055@holbertonschool.com>
2020-08-16 11:30:06 +0200

    test

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -5,8 +5,7 @@ import tensorflow as tf
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     create_layer = __import__('1-create_layer').create_layer
-    prev1 =  create_layer(x, layer_sizes[0], activations[0])
-    prev2 =  create_layer(prev1, layer_sizes[1], activations[1])
-    prev3 =  create_layer(prev2, layer_sizes[2], activations[2])
+    prev1 = create_layer(x, layer_sizes[0], activations[0])
+    prev2 = create_layer(prev1, layer_sizes[1], activations[1])
+    prev3 = create_layer(prev2, layer_sizes[2], activations[2])
     return prev3
-        
