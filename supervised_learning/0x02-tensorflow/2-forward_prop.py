cc48f5da60a204b4377dc5ede39e8806ff9543ea
yasmineholb <1055@holbertonschool.com>
2020-08-16 12:33:09 +0200

    test

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
+    """ forward prop """
     create_layer = __import__('1-create_layer').create_layer
     prev = x
     for i in range(len(layer_sizes)):
