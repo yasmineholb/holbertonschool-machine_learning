45f6768ec8ddd8f6874d2df11340687c6d0a3987
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-18 23:32:44 +0100

    Update 6-train.py

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -47,5 +47,5 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
             print("\tValidation Cost: {}".format(loss_valid))
             print("\tValidation Accuracy: {}".format(
                 accuracy_valid))
-    saver.save(sess, save_path)
+    save_path = saver.save(sess, save_path)
     return save_path
