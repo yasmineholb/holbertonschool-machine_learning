a126817606903b0b57746f0b4703578cf0616e44
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-22 21:41:17 +0200

    Update 2-shuffle_data.py

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """ shuffle_data function """
-    x = np.random.permutation(X)
-    y = np.random.permutation(Y)
-    y1 = np.random.permutation(y)
-    return x, y1
+    x = X.copy()
+    y = Y.copy()
+    np.random.shuffle(x)
+    np.random.shuffle(y)
+    return x, y
