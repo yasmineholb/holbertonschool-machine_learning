4f81e5a6d5d9f05b8c8eb51543493043003f27c5
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-23 21:35:39 +0200

    Add files via upload

---
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Adam
+Ref. Hands-On Machine Learning
+with Scikit-Learn and TensorFlow
+"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    v1 = beta1 * v + (1 - beta1) * grad
+    s1 = beta2 * s + (1 - beta2) * grad ** 2
+    v2 = (1 / (1 - (beta1 ** t))) * v1
+    s2 = (1 / (1 - (beta2 ** t))) * s1
+    var = var - (alpha * (v2 / (np.sqrt(s2) + epsilon)))
+    return var, v1, s1
