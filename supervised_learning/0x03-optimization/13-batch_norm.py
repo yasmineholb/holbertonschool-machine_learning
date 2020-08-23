30d3110b60ba268047f285eb82715b6e1e2a0a71
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-23 22:31:34 +0200

    Add files via upload

---
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Batach Normalization
+Ref. Hands-On Machine Learning
+with Scikit-Learn and TensorFlow
+"""
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    mu = np.mean(Z)
+    s = np.mean((Z - mu) ** 2)
+    z = (Z - mu) / (s + epsilon)
+    return gamma * z + beta
