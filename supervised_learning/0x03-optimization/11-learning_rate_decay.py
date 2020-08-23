eb7df305da27626709617ce97d09156b17b9310c
yasmineholb <1055@holbertonschool.com>
2020-08-23 22:17:16 +0200

    comment

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -7,5 +7,6 @@ import numpy as np
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """ learning rate """
     alpha = alpha / (1 + (decay_rate * np.floor(global_step / decay_step)))
     return alpha
