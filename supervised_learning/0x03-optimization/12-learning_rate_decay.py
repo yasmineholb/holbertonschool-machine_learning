67d585e75288630093d0ddbb3b636f7e675cd7f0
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-23 22:10:27 +0200

    Update 12-learning_rate_decay.py

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ learning decay """
-    dc = tf.train.exponential_decay(learning_rate=alpha,
+    dc = tf.train.inverse_time_decay(learning_rate=alpha,
                                     global_step=global_step,
                                     decay_steps=decay_step,
-                                    decay_rate=decay_rate, staircase=False)
+                                    decay_rate=decay_rate, staircase=True)
     """dc = alpha * decay_step ** (global_step / decay_step)"""
     return dc
