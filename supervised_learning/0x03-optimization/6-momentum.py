1733c0f60efac3d5e305b66437562068cbee469b
yasmineholb <1055@holbertonschool.com>
2020-08-23 18:51:21 +0200

    mom

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" momentum """
+import numpy as np
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """ create momentum """
+    op = tf.train.MomentumOptimizer(alpha, beta1)
+    tr = op.minimize(loss)
+    return tr
