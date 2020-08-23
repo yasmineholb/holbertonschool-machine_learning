5fdf6dd5f47760ed1228447244486b72bfb7de41
yasmineholb <1055@holbertonschool.com>
2020-08-23 20:19:31 +0200

    learn

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" learning decay
+"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """ learning decay """
+    dc = tf.train.exponential_decay(learning_rate=alpha,
+                                    global_step=global_step,
+                                    decay_steps=decay_step,
+                                    decay_rate=decay_rate)
+    return dc
