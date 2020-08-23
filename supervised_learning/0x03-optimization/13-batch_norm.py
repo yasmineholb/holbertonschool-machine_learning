cea6d8991ba75fcdd41cb41bd5c97ccdc8a34c8d
yasmineholb <1055@holbertonschool.com>
2020-08-23 22:48:53 +0200

    fix

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
+    """ batch norm """
     mu = np.mean(Z, axis=0)
     s = np.mean((Z - mu) ** 2, axis=0)
     z = (Z - mu) / np.sqrt(s + epsilon)
