e61b65df8d644e1227eae9973ec279ec8e5a2153
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-23 18:37:22 +0200

    Update 4-moving_average.py

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" shuffle_data """
+""" Moving Average"""
 import numpy as np
 
 """Ressources à regarder:
@@ -8,10 +8,9 @@ import numpy as np
 
 
 def moving_average(data, beta):
-    """ Moiving Average """
+    """ Moving Average"""
     V = [0]
-    W = []
     for i in range(len(data)):
         V.append((1 - beta) * data[i] + beta * V[i])
-        W.append(V[i+1] / (1 - beta ** (i+1)))
-    return W
+    V = np.asarray(V[1:])/np.asarray([1-beta**(i+1) for i in range(len(data))])
+    return V
