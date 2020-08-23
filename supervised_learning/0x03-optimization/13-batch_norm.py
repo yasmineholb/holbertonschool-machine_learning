bb6971f2543d23bbdb9cd1a8bce4676e2ac8e6a9
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-23 22:45:53 +0200

    Update 13-batch_norm.py

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
-    mu = np.mean(Z)
-    s = np.mean((Z - mu) ** 2)
+    mu = np.mean(Z, axis=0)
+    s = np.mean((Z - mu) ** 2, axis=0)
     z = (Z - mu) / np.sqrt(s + epsilon)
     return gamma * z + beta
