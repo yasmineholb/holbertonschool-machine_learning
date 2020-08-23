53ace8f21318169699a56282c974365b660675bd
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-23 19:32:56 +0200

    Update 5-momentum.py

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """ Momentum Grad
-Ref. Les clients ayant consulté Hands-On Machine Learning
+Ref.  Hands-On Machine Learning
 with Scikit-Learn and TensorFlow
 """
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-    """Momentum Grad"""
+    """Momentum Grad Better than asked for
     v = beta1 * v + alpha * grad
     var = var - v
+    return var, v """
+    v = beta1 * v + (1 -beta1) * grad
+    var = var - alpha * v
     return var, v
