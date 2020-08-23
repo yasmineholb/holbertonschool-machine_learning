a4622617d20e52fd4348728384f9d905fd146570
yasmineholb <1055@holbertonschool.com>
2020-08-23 20:07:27 +0200

    Adam op

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -6,5 +6,6 @@ import tensorflow as tf
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """ adam op """
-    op = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
+    op = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1,
+                                beta2=beta2, epsilon=epsilon)
     ad = op.minimize(loss)
