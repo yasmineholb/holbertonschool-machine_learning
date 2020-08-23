8744e136602c16f91296b58f43c5adb95fbdee78
yasmineholb <1055@holbertonschool.com>
2020-08-23 21:42:54 +0200

    comment

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """ update fn """
     v1 = beta1 * v + (1 - beta1) * grad
     s1 = beta2 * s + (1 - beta2) * grad ** 2
     v2 = (1 / (1 - (beta1 ** t))) * v1
