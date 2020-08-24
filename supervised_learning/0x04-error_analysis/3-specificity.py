66a1ae8913a9c7c3b23adacbe0a67ae5e14c3973
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-24 22:49:59 +0200

    Add files via upload

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Specificity matrix """
+import numpy as np
+
+
+def specificity(confusion):
+    """ specificity """
+    classes = np.shape(confusion)[0]
+    res = np.ones(classes)
+    for i in range(classes):
+        A = np.ones(np.shape(confusion))
+        A[:, i] = np.zeros(classes)
+        A[i, :] = np.zeros(classes)
+        S = sum(sum(A * confusion))
+        res[i] = S / (S + sum(confusion[1:, i]))
+    return res
