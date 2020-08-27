aef99c4d865747c87e93f71c26803ff18e235b5c
yasmineholb <1055@holbertonschool.com>
2020-08-27 19:15:28 +0200

    test

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -6,4 +6,17 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """ function that updates the weights and biases of a neural
         network using gradient descent with L2 regularization """
-    
+    copy_w = weights.copy()
+    d_z = cache["A" + str(L)] - Y
+    m = len(Y[0])
+    for i in range(L, 0, -1):
+        Actv = cache["A" + str(i - 1)]
+        d_b = (1 / m) * np.sum(d_z, axis=1, keepdims=True)
+        d_w = (1 / m) * np.matmul(d_z, Actv.T)
+        b = "b" + str(i)
+        w = "W" + str(i)
+        dw_reg = d_w + (lambtha / m) * weights[w]
+        weights[w] = weights[w] - alpha * dw_reg
+        weights[b] = weights[b] - alpha * d_b
+        da = Actv * (1 - Actv)
+        d_z = np.matmul(copy_w[w].T, d_z) * da
