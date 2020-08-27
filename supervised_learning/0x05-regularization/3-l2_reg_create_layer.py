1e3d100ac5a0c1070652dbc32b8e5c08d1db74b3
yasmineholb <1055@holbertonschool.com>
2020-08-27 22:26:02 +0200

    create layer

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -8,9 +8,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         includes L2 regularization"""
     scope = tf.variable_scope(None, default_name='layer').__enter__()
     kernel = tf.contrib.layers.l2_regularizer(lambtha)
-    init = tf.contrib.layers.variance_scaling_initializer(factor=2.0,
-                                                          mode="FAN_AVG")
-    linear_model = tf.layers.dense(prev, units=n, activation=activation,
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    linear_model = tf.layers.dense(inputs=prev, units=n, activation=activation,
                                    kernel_initializer=init,
                                    kernel_regularizer=kernel,
                                    name="layer", reuse=tf.AUTO_REUSE)
