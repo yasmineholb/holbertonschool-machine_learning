49cf6f351ce30e6b7ab19301312928e9bd5b7eec
yasmineholb <1055@holbertonschool.com>
2020-08-27 22:32:21 +0200

    regularize

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -6,12 +6,9 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """ function that creates a tensorflow layer that
         includes L2 regularization"""
-    scope = tf.variable_scope(None, default_name='layer').__enter__()
     kernel = tf.contrib.layers.l2_regularizer(lambtha)
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    linear_model = tf.layers.dense(inputs=prev, units=n, activation=activation,
+    linear_model = tf.layers.Dense(units=n, activation=activation,
                                    kernel_initializer=init,
-                                   kernel_regularizer=kernel,
-                                   name="layer", reuse=tf.AUTO_REUSE)
-    scope.reuse_variables()
-    return(linear_model)
+                                   kernel_regularizer=kernel)
+    return(linear_model(prev))
