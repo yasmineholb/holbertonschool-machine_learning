ad1f2b7816e0b0658ab49de0ec40d5d37b6e3d97
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-08-28 19:56:41 +0100

    Update 5-dropout_gradient_descent.py

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -4,21 +4,22 @@ import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """ function that updates the weights of a neural network with
-        Dropout regularization using gradient descent """
+    """ function that updates the weights of a neural network
+    with Dropout regularization using gradient descent"""
+    copy_w = weights.copy()
     d_z = cache["A" + str(L)] - Y
     m = len(Y[0])
     for i in range(L, 0, -1):
-        Actv = cache["A" + str(i - 1)]
-        d_w = (1 / m) * np.matmul(d_z, Actv.T)
-        d_b = (1 / m) * np.sum(d_z, axis=1, keepdims=True)
         b = "b" + str(i)
         w = "W" + str(i)
-        d = "D" + str(i-1)
-        if i > 1:
-            d_A = np.dot(weights[w].T, d_z)
-            d_A = np.multiply(cache[d], d_A)
-            d_A = d_A / keep_prob
-            d_z = (1-(Actv**2)) * d_A
-        weights[b] = weights[b] - alpha * d_b
+        d = "D" + str(i - 1)
+        Actv = cache["A" + str(i - 1)]
+        d_b = (1 / m) * np.sum(d_z, axis=1, keepdims=True)
+        d_w = (1 / m) * np.matmul(d_z, Actv.T)
         weights[w] = weights[w] - alpha * d_w
+        weights[b] = weights[b] - alpha * d_b
+        da = 1 - Actv * Actv
+        if i > 1:
+            d_z = np.matmul(copy_w[w].T, d_z) * da * cache[d] / keep_prob
+        else:
+            d_z = np.matmul(copy_w[w].T, d_z) * da
