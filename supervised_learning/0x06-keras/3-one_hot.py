a4425c6815f4930a9c7ecce0b527c4d8cbdb16be
yasmineholb <1055@holbertonschool.com>
2020-08-31 21:00:31 +0200

    keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """ One hot """
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """ Function  that converts a label
         vector into a one-hot matrix """
-    b = np.zeros((labels.size, labels.max()+1))
-    b[np.arange(labels.size), labels] = 1
-    return b
+    return K.utils.to_categorical(labels, classes)
