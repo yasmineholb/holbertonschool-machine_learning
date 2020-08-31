c2b314d64b783f0318f427c11ab2c5a4ac991d57
yasmineholb <1055@holbertonschool.com>
2020-08-31 21:57:56 +0200

    patience

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" train model """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """ Function that train the model using early stopping """
+    if early_stopping is True:
+        if validation_data is not None:
+            es = K.callbacks.EarlyStopping(monitor='val_loss',
+                                           mode='min', patience=patience)
+            history = network.fit(x=data, y=labels, epochs=epochs,
+                                  verbose=verbose,
+                                  batch_size=batch_size,
+                                  validation_data=validation_data,
+                                  shuffle=shuffle, callbacks=[es])
+    else:
+        history = network.fit(x=data, y=labels, epochs=epochs, verbose=verbose,
+                              batch_size=batch_size,
+                              validation_data=validation_data,
+                              shuffle=shuffle)
+    return history
