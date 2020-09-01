198abef79f34b2bb51c23368768e0c9f12bbe312
yasmineholb <1055@holbertonschool.com>
2020-09-01 13:59:06 +0200

    test

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -9,14 +9,17 @@ def train_model(network, data, labels, batch_size, epochs,
                 alpha=0.1, decay_rate=1, verbose=True,
                 shuffle=False):
     """ Function that train the model with learning rate decay """
-    if validation_data and patience < epochs:
-        tf = []
+    if validation_data:
         if learning_rate_decay is True:
             def scheduler(epoch):
                 """ scheduler Function """
                 return alpha / (1 + decay_rate * epoch)
             call = K.callbacks.LearningRateScheduler(scheduler, 1)
-            tf.append(call)
+            history = network.fit(x=data, y=labels, epochs=epochs,
+                                  verbose=verbose,
+                                  batch_size=batch_size,
+                                  validation_data=validation_data,
+                                  shuffle=shuffle, callbacks=[call])
     if early_stopping is True:
         if validation_data is not None and patience < epochs:
             es = K.callbacks.EarlyStopping(monitor='val_loss',
