d480102e244f84be0cae2248c737e0fa59f033b0
yasmineholb <1055@holbertonschool.com>
2020-09-01 22:04:38 +0200

    see

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -7,7 +7,8 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """ Function that trains a model using mini-batch
         gradient descent """
-    history = network.fit(data, labels,
+    history = network.fit(data,
+                          labels,
                           batch_size=batch_size,
                           epochs=epochs,
                           verbose=verbose,
