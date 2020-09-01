aa2f4c75782a75349628e37891af39d98f436bec
yasmineholb <1055@holbertonschool.com>
2020-09-01 15:16:47 +0200

    test

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" test model """
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """ Function that tests a neural network """
+    test = nework.evaluate(data, labels, verbose=verbose)
+    return test
