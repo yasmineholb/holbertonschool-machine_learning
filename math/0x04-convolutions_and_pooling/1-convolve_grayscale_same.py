9f266abe620146fac7b9cbfe9abf98d7ba875793
yasmineholb <1055@holbertonschool.com>
2020-09-04 01:29:19 +0200

    test

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -11,16 +11,11 @@ def convolve_grayscale_same(images, kernel):
     output_h = h - kh + 1
     output_w = w - kw + 1
     t = np.arange(m)
-    pad_along_height = max((output_h - 1) + kh - h, 0)
-    pad_along_width = max((output_w - 1) + kw - w, 0)
-    pad_top = pad_along_height // 2             # amount of zero padding on the top
-    pad_bottom = pad_along_height - pad_top     # amount of zero padding on the bottom
-    pad_left = pad_along_width // 2             # amount of zero padding on the left
-    pad_right = pad_along_width - pad_left      # amount of zero padding on the right
-    output = np.zeros((m, output_h, output_w))  # convolution output
-    # Add zero padding to the input image
-    image_padded = np.zeros((m, h + pad_along_height,
-                            w + pad_along_width))
+    output = np.zeros((m, output_h, output_w))
+    image_padded = np.zeros((m, (h + (kh-1)), 
+                             (w + (kw-1))))   
+    image_padded[t, (kh//2):-(kh//2), 
+                 (kw//2):-(kw//2)] = images
     """image_padded[m, pad_top:-pad_bottom, pad_left:-pad_right, :] = conv_input"""
     for x in range(output_h):
         for y in range(output_w):
