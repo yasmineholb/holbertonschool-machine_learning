b096a794c84748510750c7df3c9d01f975c84f14
yasmineholb <1055@holbertonschool.com>
2020-09-04 22:48:20 +0200

    test

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -9,19 +9,21 @@ def convolve_grayscale(images, kernel, padding='same',
         grayscale images """
     m, h, w = np.shape(images)
     kh, kw = np.shape(kernel)
+    sh, sw = stride
     if padding == "valid":
         ph = 0
         pw = 0
     elif padding == "same":
-        ph = (kh-kh % 2)//2
-        pw = (kw-kw % 2)//2
+        """ph = (kh-kh % 2)//2
+        pw = (kw-kw % 2)//2"""
+        ph = int(((h - 1) * sh + kh - h) / 2) + 1
+        pw = int(((w - 1) * sw + kw - w) / 2) + 1
     else:
         ph, pw = padding
-    sh, sw = stride
     image = np.zeros((m, (h + 2 * ph), (w + 2 * pw)))
     image[:, ph:h+ph, pw:w+pw] = images.copy()
-    nh = np.floor((h + 2 * ph - kh) / stride[0] + 1).astype(int)
-    nw = np.floor((w + 2 * pw - kw) / stride[1] + 1).astype(int)
+    nh = np.floor(((h + 2 * ph - kh) / stride[0]) + 1).astype(int)
+    nw = np.floor(((w + 2 * pw - kw) / stride[1]) + 1).astype(int)
     S = np.zeros((m, nh, nw))
     im = np.arange(0, m)
     for i in range(nh):
