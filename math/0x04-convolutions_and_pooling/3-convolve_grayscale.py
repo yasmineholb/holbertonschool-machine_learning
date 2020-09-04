1f2881178676b2144c0e0d5824a24582783fd172
yasmineholb <1055@holbertonschool.com>
2020-09-04 19:02:25 +0200

    test

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -13,25 +13,19 @@ def convolve_grayscale(images, kernel, padding='same',
         ph = 0
         pw = 0
     elif padding == "same":
-        if kh % 2 == 1:
-            ph = (kh-1)//2
-        else:
-            ph = kh//2
-        if kw % 2 == 1:
-            pw = (kw-1)//2
-        else:
-            pw = kw//2
+        ph = (kh-kh % 2)//2
+        pw = (kw-kw % 2)//2
     else:
         ph, pw = padding
     sh, sw = stride
     image = np.zeros((m, (h + 2 * ph), (w + 2 * pw)))
     image[:, ph:h+ph, pw:w+pw] = images.copy()
-    t1, h1, w1 = np.shape(image)
-    S = np.zeros((m, ((h + 2 * ph - kh) // stride[0]) + 1,
-                  ((w + 2 * pw - kw) // stride[1]) + 1))
+    nh = np.floor((h + 2 * ph - kh) / stride[0] + 1).astype(int)
+    nw = np.floor((w + 2 * pw - kw) / stride[1] + 1).astype(int)
+    S = np.zeros((m, nh, nw))
     im = np.arange(0, m)
-    for i in range(((h + 2 * ph - kh) // stride[0]) + 1):
-        for j in range(((w + 2 * pw - kw) // stride[1]) + 1):
+    for i in range(nh):
+        for j in range(nw):
             S[im, i, j] = np.sum(image[im,
                                        sh * i:sh * i + kh,
                                        sw * j:sw * j + kw] * kernel,
