2e1b6e254f8839cfa1f2c8db73e12facd781ec2b
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-09-09 22:58:31 +0100

    Add files via upload

---
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" convolve function """
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    m, h, w, c = np.shape(A_prev)
+    kh, kw, c, nc = np.shape(W)
+    sh, sw = stride
+    if padding == "valid":
+        ph = 0
+        pw = 0
+    elif padding == "same":
+        ph = int(((h - 1) * sh + kh - h) / 2) + 1
+        pw = int(((w - 1) * sw + kw - w) / 2) + 1
+    else:
+        ph, pw = padding
+    image = np.zeros((m, (h + 2 * ph), (w + 2 * pw), c))
+    image[:, ph:h+ph, pw:w+pw, :] = A_prev.copy()
+    nh = np.floor(((h + 2 * ph - kh) / stride[0]) + 1).astype(int)
+    nw = np.floor(((w + 2 * pw - kw) / stride[1]) + 1).astype(int)
+    S = np.zeros((m, nh, nw, nc))
+    im = np.arange(0, m)
+    for i in range(nh):
+        for j in range(nw):
+            for k in range(nc):
+                S[im, i, j, k] += np.sum(image[im, sh * i:sh * i + kh,
+                                               sw * j:sw * j + kw, :]
+                                         * W[:, :, :, k],
+                                         axis=(1, 2, 3))
+    return activation(S + b)
