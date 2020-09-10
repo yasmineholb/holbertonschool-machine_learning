b14b4fe9bdae476dadc706ff8cdce5ee49789254
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-09-10 23:55:26 +0200

    Add files via upload

---
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Pooling backward Prop """
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """ Function that performs backward propagation over
+        a pooling layer of a neural network """
+    m, h_new, w_new, c_new = dA.shape
+    m, h_prev, w_prev, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    dA_prev = np.zeros(A_prev.shape)
+    
+    for i in range(m):                       
+        a_prev = A_prev[i]
+        for h in range(h_new):                   
+            for w in range(w_new):               
+                for c in range(c_new):   
+                    
+                    h_start  = h * sh
+                    h_end    = h_start + kh
+                    w_start = w * sw
+                    w_end   = h_start + kw
+                    
+                    if mode == "max":
+                        a_prev_slice = a_prev[h_start:h_end, w_start:w_end, c]
+                        mask = (a_prev_slice == np.max(a_prev_slice))
+                        dA_prev[i, h_start:h_end, w_start:w_end, c] += mask * dA[i, h, w, c]
+                    
+                    elif mode == "average":
+                        da = dA[i, h, w, c]
+                        shape = (kh, kw)
+                        average = da / (kh * kw) * np.ones(shape)
+                        dA_prev[i, h_start:h_end, w_start:w_end, c] += average
+    return dA_prev
