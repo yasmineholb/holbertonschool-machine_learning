7b942ae2716fd89ca4a3712e868456ea075f5c2d
yasmineholb <1055@holbertonschool.com>
2020-09-11 00:22:15 +0200

    test

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -10,25 +10,21 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     m, h_prev, w_prev, c = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
-
     dA_prev = np.zeros(A_prev.shape)
-    
-    for i in range(m):                       
+    for i in range(m):
         a_prev = A_prev[i]
-        for h in range(h_new):                   
-            for w in range(w_new):               
-                for c in range(c_new):   
-                    
-                    h_start  = h * sh
-                    h_end    = (h * sh) + kh
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    h_start = h * sh
+                    h_end = (h * sh) + kh
                     w_start = w * sw
-                    w_end   = (w * sw) + kw
-                    
+                    w_end = (w * sw) + kw
                     if mode == "max":
                         a_prev_slice = a_prev[h_start:h_end, w_start:w_end, c]
                         mask = (a_prev_slice == np.max(a_prev_slice))
-                        dA_prev[i, h_start:h_end, w_start:w_end, c] += mask * dA[i, h, w, c]
-                    
+                        dA_prev[i, h_start:h_end,
+                                w_start:w_end, c] += mask * dA[i, h, w, c]
                     elif mode == "average":
                         da = dA[i, h, w, c]
                         shape = (kh, kw)
