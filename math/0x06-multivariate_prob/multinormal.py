b619edb341a988b63e8e552daaf57ab613dff5f5
yasmineholb <1055@holbertonschool.com>
2020-10-26 21:01:42 +0100

    multi

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """ Multinormal """
+import numpy as np
+
 
 class MultiNormal:
     """ Class Multinormal """
     def __init__(self, data):
-        """ Funtion that represents a Multivariate 
+        """ Funtion that represents a Multivariate
             Normal distribution """
-        
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points")
+        d, n = data.shape
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        xi = data - self.mean
+        self.cov = np.matmul(xi, xi.T) / (n - 1)
