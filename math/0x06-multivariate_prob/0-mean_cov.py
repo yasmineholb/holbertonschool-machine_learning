d9b2483966eb16608fae8e65c0685ea2b1aa0104
yasmineholb <1055@holbertonschool.com>
2020-10-26 19:25:39 +0100

    mean conv

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -9,7 +9,7 @@ def mean_cov(X):
     n, d = X.shape
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    if n < 2:
+    if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0, keepdims=True)
     xi = X - mean
