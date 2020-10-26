5f061d2e5296fb399f8d68e71d6bdebffa9636ad
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-10-26 20:14:02 +0100

    Update 1-correlation.py

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -4,3 +4,16 @@
 
 def correlation(C):
     """ Function that calculates a correlation matrix """
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+        raise TypeError("C must be a 2D numpy.ndarray")
+    if C.shape[0] < 2:
+        raise ValueError("C must contain multiple data points")
+    n, d = C.shape
+    if n != d:
+        raise ValueError('C must be a 2D square matrix')
+    M = np.ones((n, n))
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                M[i, j] = C[i, j] / np.sqrt(abs(C[i, i] * C[j, j]))
+    return M
