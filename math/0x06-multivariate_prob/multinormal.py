b5bab077c5dc545027105c65f26c130e75991397
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-10-26 21:29:38 +0100

    Update multinormal.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -16,12 +16,20 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         xi = data - self.mean
         self.cov = np.matmul(xi, xi.T) / (n - 1)
-
+        
+        
     def pdf(self, x):
-        """ Function that calculates the PDF at a data point """
+        """ Funtion that clculates the PDF of a Multivariate
+            Normal distribution """
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
-        d, n = x.shape
-        if len(x.shape) != 2 or x.shape[0] != d.shape[1] != 1:
+        d = self.mean.shape[0]
+        if len(x.shape) != 2:
             raise ValueError("x must have the shape ({d}, 1)")
-        
+        if x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError("x must have the shape ({d}, 1)")
+        K = (1 / (((2 * np.pi) ** d) * np.linalg.det(self.cov)) ** (0.5))
+        M1 = np.matmul((x - self.mean).T, np.linalg.inv(self.cov))
+        M = -0.5 * np.matmul(M1,(x - self.mean))
+        return K * np.exp(M)[0, 0]
+    
