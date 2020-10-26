e428690ec3b2916be1f512304d4c332dd9441276
yasmineholb <1055@holbertonschool.com>
2020-10-26 19:21:33 +0100

    mean conv

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -11,7 +11,7 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     if n < 2:
         raise ValueError("X must contain multiple data points")
-    mean = np.mean(X, axis=0)
+    mean = np.mean(X, axis=0, keepdims=True)
     xi = X - mean
-    cov = np.dot(xi.T, xi) / (n - 1)
+    cov = np.matmul(xi.T, xi) / (n - 1)
     return mean, cov
