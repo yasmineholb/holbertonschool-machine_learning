eb52953c536e81e72061744bee54d0ae122f364a
yasmineholb <1055@holbertonschool.com>
2020-10-26 20:24:22 +0100

    test2

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -7,8 +7,7 @@ def correlation(C):
     """ Function that calculates a correlation matrix """
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    p, x = C.shape
-    if len(C.shape) != 2 or p != x:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     d = np.diag(C)
     ch = d.reshape(-1, 1)
