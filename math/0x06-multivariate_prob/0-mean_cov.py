f2aab9f48005f4aa056dcd46e3fdef5b918ba446
yasmineholb <1055@holbertonschool.com>
2020-10-26 19:12:24 +0100

    mean conv

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """ mean_cov """
+import numpy as np
 
 
 def mean_cov(X):
     """ Function that calculates the mean
         and covariance of a data set """
+    n, d = X.shape
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=0)
+    xi = X - mean
+    cov = np.matmul(xi.T, xi) / (n - 1)
+    return mean, cov
