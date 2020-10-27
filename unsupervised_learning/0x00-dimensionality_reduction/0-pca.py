51d0df1c88025f31661e1dc4f833c9e4fc2363eb
yasmineholb <1055@holbertonschool.com>
2020-10-27 20:28:19 +0100

    pca

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" pca """
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """ Function that performs PCA on a dataset: """
+    tab = np.matmul(X.T, X)
+    V = np.linalg.eig(tab)[0]
+    V = abs(V)
+    V = V / sum(V)
+    la = []
+    S = 0
+    while S < var:
+        S += V[np.argmax(V)]
+        la.append(np.argmax(V))
+        V[np.argmax(V)] = -1
+    return np.linalg.eig(tab)[1][:, la]
