45f639405772978204c928d4cc3d4e0f2e968be5
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-10-29 22:08:48 +0100

    Update 3-entropy.py

--- unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-""" HP """
+""" Entropy """
 import numpy as np
 
 
 def HP(Di, beta):
-    """ Function that calculates the Shannon
-        entropy and P affinities relative to a data point """
-    P = np.exp(-Di.copy() * beta)
-    sumP = sum(P)
-    H = np.log2(sumP) + beta * np.sum(Di * P) / sumP
-    P = P / sumP
-    return H, P
+    """function that calculates the Shannon entropy3
+    and P affinities relative to a data point"""
+    Pi = np.exp(- Di * beta)
+    Pi /= np.sum(Pi)
+    Hi = - np.sum(Pi * np.log2(Pi))
+    return Hi, Pi
