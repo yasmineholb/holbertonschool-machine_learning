4648c7a3dd8c9160c438fdf4cf4cca5a677f69be
yasmineholb <1055@holbertonschool.com>
2020-10-29 21:28:14 +0100

    test

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -5,13 +5,10 @@ import numpy as np
 def P_init(X, perplexity):
     """ initializes all variables required 
     to calculate the P affinities in t-SNE"""
-    n, d = np.shape(X)
-    D = np.zeros((n, n))
-    for i in range(n):
-        for j in range(i):
-            D[i, j] = np.linalg.norm(X[i, :] - X[j, :])
-    D += D.T
+    (n, d) = X.shape
+    sum_X = np.sum(np.square(X), 1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
     P = np.zeros((n, n))
-    betas = np.ones((n, 1))
-    H = np.log2(perplexity)
-    return D, P, betas, H
+    beta = np.ones((n, 1))
+    logU = np.log(perplexity)
+    return D, P, beta, logU
