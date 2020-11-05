0a60155ce2815a5159e7950d46c5231c235c8d11
yasmineholb <1055@holbertonschool.com>
2020-11-05 19:13:20 +0100

    gmm

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -6,8 +6,7 @@ import sklearn.mixture
 def gmm(X, k):
     """ Function that calculates a
         GMM from a dataset """
-    gauss = sklearn.mixture.GaussianMixture(n_components=k)
-    gauss = gauss.fit(X)
+    gauss = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
     pi = gauss.weights_
     m = gauss.means_
     S = gauss.covariances_
