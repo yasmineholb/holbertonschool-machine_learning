1388ef21f9cb23266c0b76afa98faaf1c226aa13
yasmineholb <1055@holbertonschool.com>
2020-11-11 22:03:25 +0100

    markov

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -7,6 +7,10 @@ def markov_chain(P, s, t=1):
     """ Function that determines the probability of a
         markov chain being in a particular state after
         a specified number of iterations """
+    if type(P) is not np.ndarray or len(P.shape) != 2:
+        return None
+    if len(s.shape) != 2 or s.shape[0] != 1:
+        return None
     for i in range(t):
         s = np.matmul(s, P)
     if s is not None:
