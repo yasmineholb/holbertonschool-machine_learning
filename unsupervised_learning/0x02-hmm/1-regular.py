c39d6491efc768af073d2e243dadf0278d5061f2
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-11-11 23:32:05 +0100

    Add files via upload

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" markov_chain """
+import numpy as np
+
+
+def regular(P):
+    """ determines the steady state
+    probabilities of a regular markov chain """
+    if type(P) is not np.ndarray or len(P.shape) != 2:
+        return None
+    VP = np.linalg.eig(np.transpose(P))
+    s = np.argmax(VP[0])
+    if np.allclose(VP[0][s], 1):
+        b = np.sort(VP[0])
+        if np.allclose(b[-1], b[-2]):
+            return None
+        else:
+            a = np.abs(np.transpose(VP[1][:, s]))
+            return a / np.sum(a)
+    else:
+        return None
