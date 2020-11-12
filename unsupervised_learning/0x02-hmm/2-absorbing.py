88d66aea967780e3876c678c2f40f8458ccf8c42
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-11-12 23:02:56 +0100

    Update 2-absorbing.py

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -7,19 +7,38 @@ def absorbing(P):
     """function that determines if a markov chain is absorbing"""
     if type(P) is not np.ndarray or len(P.shape) != 2:
         return None
-    if P.shape[0] != P.shape[1]:
-        return None
-    s = 0
-    for i in range(len(P)):
-        if P[i][i] == 1:
-            s += 1
-    if s == len(P):
-        return True
-    if s == 0:
+    e = np.diag(P)
+    n = len(P)
+    if np.max(e) != 1:
         return False
-    for m in range(len(P)):
-        for n in range(len(P)):
-            if (m == n) and (m + 1 < len(P)):
-                if P[m + 1][n] == 0 and P[m][n + 1] == 0:
-                    return False
-    return True
+    elif (P == np.eye(n)).all():
+        return True
+    else:
+        u = np.where(e == 1)
+        j = 0
+        for i in u[0]:
+            while(P[n - 1 - j, n - 1 - j] == 1):
+                j += 1
+            if i < n - j - 1:
+                S = np.eye(n)
+                S[i, i] = 0
+                S[n - j - 1, n - j - 1] = 0
+                S[i, n - j - 1] = 1
+                S[n - j - 1, i] = 1
+                P = np.matmul(S, np.matmul(P, S))
+                j += 1
+        j = len(u[0])
+        Q = P[0:n - j, 0:n - j]
+        R = P[0:n - j, n - j:n]
+        QI = np.eye(n - j) - Q
+        if np.linalg.det(QI) == 0:
+            return False
+        else:
+            B = np.matmul(np.linalg.inv(QI), R)
+            test = 0
+            for i in range(j):
+                if (B[:, i] == 0).any():
+                    test += 1
+            if test == j:
+                return False
+            return True
