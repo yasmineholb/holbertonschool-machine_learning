d207bba969afa0033891b2d1e893a045f8c24d08
yasmineholb <1055@holbertonschool.com>
2020-11-12 13:20:03 +0100

    test

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -7,27 +7,19 @@ def absorbing(P):
     """function that determines if a markov chain is absorbing"""
     if type(P) is not np.ndarray or len(P.shape) != 2:
         return None
-    e = np.diag(P)
-    n = len(P)
-    if np.max(e) != 1:
-        return False
-    elif (P == np.eye(n)).all():
+    if P.shape[0] != P.shape[1]:
+        return None
+    s = 0
+    for i in range(len(P)):
+        if P[i][i] == 1:
+            s += 1
+    if s == len(P):
         return True
-    else:
-        u = np.where(e == 1)
-        j = 0
-        for i in u[0]:
-            S = np.eye(n)
-            S[i, i] = 0
-            S[n - j - 1, n - j - 1] = 0
-            S[i, n - j - 1] = 1
-            S[n - j - 1, i] = 1
-            P = np.dot(S, np.dot(P, S))
-            j += 1
-        Q = P[0:n - j, 0:n - j]
-        R = P[0:n - j, n - j:n]
-        QI = np.eye(n - j) - Q
-        if np.linalg.det(QI) == 0:
-            return False
-        else:
-            return True
+    if s == 0:
+        return False
+    for m in range(len(P)):
+        for n in range(len(P)):
+            if (m == n) and (m + 1 < len(P)):
+                if P[m + 1][n] == 0 and P[m][n + 1] == 0:
+                    return False
+    return True
