5f58b961bcfae688b09b05396c4876309bf41c16
HAMDI - <n419676@santanderglobaltech.com>
2021-08-02 19:57:14 +0200

    DB

---
+++ pipeline/0x02-databases/34-log_stats.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Log stats"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """ Python script that provides some stats about Nginx logs
+        stored in MongoDB """
+    print("{} status check".format())
