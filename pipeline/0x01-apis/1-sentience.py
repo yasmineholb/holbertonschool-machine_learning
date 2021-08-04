de5de04903e2b65261e40571b578878a7295330b
HAMDI - <n419676@santanderglobaltech.com>
2021-08-04 14:30:13 +0200

    species

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Where I am? """
+import requests
+
+
+def sentientPlanets():
+    """ method that returns the list of names
+    of the home planets of all sentient species """
+    Hplanets = []
+    req = 'https://swapi-api.hbtn.io/api/species/'
+    while req is not None:
+        response = requests.get(req, headers={'Accept': 'application/json'})
+        for pl in response.json()['results']:
+            if pl["homeworld"] != "n/a" and pl["homeworld"] is not None:
+                Hworls = prequests.get(specie['homeworld'])
+                Hplanets.append(homeworld.json()['name'])
+        req = response.json()['next']
+    return Hplanets
