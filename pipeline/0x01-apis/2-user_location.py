0289d524f2017b10bcd49ee75f9b72a1a02d334e
yasmineholb <yasmineholb@github.com>
2021-09-23 17:53:09 +0200

    location

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Rate me is you can! """
+import sys
+import time
+import requests
+
+
+if __name__ == '__main__':
+    ln = sys.argv[1]
+    ld = {'Accept': 'application/vnd.github.v3+json'}
+    req = requests.get(ln, params=ld)
+    if req.status_code == 200:
+        location = req.json()["location"]
+        print(location)
+    if req.status_code == 403:
+        limit = req.headers["X-Ratelimit-Reset"]
+        x = (int(limit) - int(time.time())) / 60
+        print("Reset in {} min".format(int(x)))
+    if req.status_code == 404:
+        print("Not found")
