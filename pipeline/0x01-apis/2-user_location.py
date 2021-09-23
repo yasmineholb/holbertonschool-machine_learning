65b040fb269efb4154211c41c0ed4301d68e7656
yasmineholb <yasmineholb@github.com>
2021-09-23 18:08:37 +0200

    location

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -7,13 +7,16 @@ import requests
 
 if __name__ == '__main__':
     ln = sys.argv[1]
-    req = requests.get(ln, params=ld)
+    req = requests.get(ln)
     js = r.json()
     if req.status_code == 403:
         lt = req.headers["X-Ratelimit-Reset"]
         x = (int(lt) - int(time.time())) / 60
         print("Reset in {} min".format(int(x)))
     if req.status_code == 200:
-        print(js["location"])
+        try:
+            print(js["location"])
+        except KeyError:
+            print("Not found")
     if req.status_code == 404:
         print("Not found")
