f4d228784d2066e881d3c60fb855f56d5d55e637
yasmineholb <yasmineholb@github.com>
2021-09-23 18:05:44 +0200

    location

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -7,14 +7,13 @@ import requests
 
 if __name__ == '__main__':
     ln = sys.argv[1]
-    ld = {'Accept': 'application/vnd.github.v3+json'}
     req = requests.get(ln, params=ld)
+    js = r.json()
     if req.status_code == 403:
         lt = req.headers["X-Ratelimit-Reset"]
         x = (int(lt) - int(time.time())) / 60
         print("Reset in {} min".format(int(x)))
     if req.status_code == 200:
-        location = req.json()["location"]
-        print(location)
+        print(js["location"])
     if req.status_code == 404:
         print("Not found")
