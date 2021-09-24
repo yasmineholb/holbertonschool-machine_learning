52aa4581388005e7d36b1944626ed16fdd61b009
yasmineholb <yasmineholb@github.com>
2021-09-24 10:15:23 +0200

    new

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
-'''2. Rate me is you can!'''
+""" Rate me is you can! """
 import sys
 import requests
-from datetime import datetime
-
-
-def loc(url):
-    '''returns the location of a specific user'''
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    request = requests.get(url)
-    if request.status_code == 403:
-        retry = request.headers['X-Ratelimit-Reset']
-        seconds = int(retry) - int(datetime.now().timestamp())
-        print('Reset in {} min'.format(seconds // 60))
-    elif request.status_code == 404:
-        print('Not found')
-    elif request.status_code == 200:
-        print(request.json()['location'])
+import time
 
 
 if __name__ == '__main__':
-    loc(sys.argv[1])
+
+    ln = sys.argv[1]
+    js = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(ln, params=js)
+    if r.status_code == 403:
+        limit = r.headers["X-Ratelimit-Reset"]
+        x = (int(limit) - int(time.time())) / 60
+        print("Reset in {} min".format(int(x)))
+    if r.status_code == 200:
+        loc = r.json()["location"]
+        print(loc)
+    if r.status_code == 404:
+        print("Not found")
