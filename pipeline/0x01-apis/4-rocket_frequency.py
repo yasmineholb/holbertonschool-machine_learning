785845122582fe7270a438c2e00e5103f7a38bca
yasmineholb <yasmineholb@github.com>
2021-09-24 10:23:11 +0200

    new

---
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" How many by rocket? """
+import requests
+
+
+if __name__ == '__main__':
+    ln = "https://api.spacexdata.com/v4/launches"
+    r = requests.get(ln)
+    launches = r.json()
+    dict = {}
+    for i, launch in enumerate(launches):
+        rk_id = launch["rocket"]
+        rk_url = "https://api.spacexdata.com/v4/rockets/{}".format(
+            rk_id)
+        rocket = requests.get(rk_url).json()["name"]
+        if rocket in dict.keys():
+            dict[rocket] += 1
+        else:
+            dict[rocket] = 1
+    rk = sorted(dict.items(), key=lambda kv: kv[0])
+    rk = sorted(rk, key=lambda kv: kv[1], reverse=True)
+    for rocket in rk:
+        print(*rocket, sep=": ")
