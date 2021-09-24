d4191b421267813a6cab23716741fb0767c11594
yasmineholb <yasmineholb@github.com>
2021-09-24 10:19:09 +0200

    new

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python3
-
-
-""" script that displays the upcoming
-    launch with these information """
-
+""" What will be next? """
 import requests
 
 
-if __name__ == "__main__":
-    url = "https://api.spacexdata.com/v4/launches/upcoming"
-    r = requests.get(url)
-
-    values = r.json()
-    values.sort(key=lambda x: x["date_unix"])
-    value = values[0]
-
-    # launch and date
-    launch_name = value["name"]
-    date = value["date_local"]
-
-    # rocket
-    rocket_id = value["rocket"]
-    r = requests.get(
-        "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id))
-    rocket_values = r.json()
-    rocket_name = rocket_values["name"]
-
-    # launchpad
-    launchpad_id = value["launchpad"]
-    r = requests.get(
-        "https://api.spacexdata.com/v4/launchpads/{}".format(launchpad_id))
-    launchpad_values = r.json()
-    launchpad_name = launchpad_values["name"]
-    launchpad_locality = launchpad_values["locality"]
-
-    print("{} ({}) {} - {} ({})".format(launch_name, date,
-                                        rocket_name, launchpad_name,
-                                        launchpad_locality))
+if __name__ == '__main__':
+    ln = "https://api.spacexdata.com/v4/launches/upcoming"
+    r = requests.get(ln)
+    js = r.json()
+    date = float('inf')
+    for i, launch in enumerate(js):
+        if date > launch["date_unix"]:
+            date = launch["date_unix"]
+            index = i
+    n = js[index]["name"]
+    d = js[index]["date_local"]
+    r_id = js[index]["rocket"]
+    ln = "https://api.spacexdata.com/v4/rockets/{}".format(r_id)
+    r_name = requests.get(ln).json()["name"]
+    lp_id = js[index]["launchpad"]
+    ln = "https://api.spacexdata.com/v4/launchpads/{}".format(lp_id)
+    lp = requests.get(ln).json()
+    lp_name = lp["name"]
+    lp_loc = lp["locality"]
+    print(n + " (" + d + ") " + r_name + " - " + lp_name + " (" + lp_loc + ")")
